0f3d7738f1d0f1e8022a811c1ed09bfdf83a8625
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-10-17 14:44:29 -0500

    Add files via upload

---
+++ product.js
@@ -0,0 +1,22 @@
+const product = functions(){
+let name = n;
+let price = p;
+let space = s;
+let prime = pm;
+
+function makeName(){
+    for(x=0;x<10;x++){
+        let name = "";
+        let c = Math.floor(25*Math.random())+65;
+        let letter = String.fromCharCode();
+        let name += letter;
+          }
+     }
+ function makePrice(){
+     for(x-0;x<10;x++){
+         let price = Math.floor(Math.random()*10000/100);
+         let space = Math.floor(Math.random()*10000/100);
+
+     }
+ }
+}
